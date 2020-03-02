24a021d3aeb36d55ae4d354a86a11eff883a0fd0
Vishwas Suryanarayanan <sparkvish1@gmail.com>
2020-03-02 10:36:09 -0800

    added notebook and made final changes to embedder.

--- embedder/embedder.py
+++ embedder/embedder.py
@@ -1,22 +1,30 @@
 import tensorflow as tf
 import tensorflow_hub as hub
-
+import spacy
+import tqdm
 
 class Embedder():
         
     def __init__(self, url='https://tfhub.dev/google/elmo/3'):
         self.embedder = hub.Module(url)
-    
+        self.nlp = spacy.load('en_core_web_md')
+        
     def preprocess(self, text):
+        text = [doc.replace('\n', ' ').replace('\t', ' ').replace('\xa0', ' ') for doc in tqdm.tqdm( text)]
+        text =  [' '.join(doc.split())for doc in tqdm.tqdm(text)]
         return text
 
-    def tokenize(self, text):
-        tokens = text.replace('\n', ' ').replace('\t', ' ').replace('\xa0', ' ')
-        tokens = ' '.join(tokens.split()).split(' ')
-        return tokens
+    def doc_tokenize(self, text):
+        return [self.nlp(doc).char_span(0, len(doc)).string.strip() for doc in tqdm.tqdm(text)]
 
-    def embed(self, tokens):
-        embedding = self.embedder(tokens,
+    def sent_tokenize(self, text):
+        tokens = [doc.replace('\n', ' ').replace('\t', ' ').replace('\xa0', ' ') for doc in text]
+        tokens = [' '.join(doc.split()).split(' ') for doc in tokens]
+        tokens = [doc.sents for doc in tokens]
+        return tokens
+    
+    def doc_embed(self, doc_tokens):
+        embedding = self.embedder(doc_tokens,
                              signature='default',
                              as_dict=True)['default']
         with tf.Session() as sess:
